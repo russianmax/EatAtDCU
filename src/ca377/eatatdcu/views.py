0fd24ce4d4eeac125f4bf4973558b68ef83b9ee4
jfoster <jennifer.foster@dcu.ie>
2018-09-04 14:00:05 +0100

    A0 assignment

--- src/ca377/eatatdcu/views.py
+++ src/ca377/eatatdcu/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to Eat@DCU")
-
+    template = loader.get_template('eatatdcu/index.html')
+    return HttpResponse(template.render({},request))
