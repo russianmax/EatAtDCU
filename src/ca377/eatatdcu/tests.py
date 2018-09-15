88331991a284df2e0557235890a1c2b9382e9a8c
Jennifer Foster <jenniferfoster@Jennifers-MacBook-Air.local>
2018-09-15 13:36:11 +0100

    One test case

--- src/ca377/eatatdcu/tests.py
+++ src/ca377/eatatdcu/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class IndexTests(TestCase):
+
+    def test_welcome(self):
+        """
+        An appropriate welcome message is displayed once the page is loaded
+        """
+        response = self.client.get(reverse('eatatdcu:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Eat at DCU!")
+
+
