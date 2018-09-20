44b96ac28429fb6b708546bd3663a18b9332b52c
jfoster <jennifer.foster@dcu.ie>
2018-09-20 12:37:40 +0100

    updated tests for A0

--- src/ca377/eatatdcu/tests.py
+++ src/ca377/eatatdcu/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.core.urlresolvers import get_resolver
 
-class IndexTests(TestCase):
+class A0Tests(TestCase):
    
     def test_welcome(self):
       """
@@ -10,3 +11,20 @@ class IndexTests(TestCase):
       response = self.client.get(reverse('eatatdcu:index'))
       self.assertEqual(response.status_code, 200)
       self.assertContains(response, "Eat at DCU!")
+
+    def test_restaurants(self):
+      """
+      An appropriate message is displayed once the restaurants page is loaded
+      """
+      response = self.client.get(reverse('eatatdcu:restaurants'))
+      self.assertEqual(response.status_code, 200)
+      self.assertContains(response, "No restaurants found")
+
+    def test_index_restaurants(self):
+      """
+      The index page refers to the restaurants 
+      """
+      response = self.client.get(reverse('eatatdcu:index'))
+      self.assertContains(response, "Restaurants")
+
+
