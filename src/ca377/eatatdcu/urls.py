4ee61a1662f3e544810afbdff4aed6588e8eb9c9
Maksims Kompanijecs <kompanijecs.maksims2@mail.dcu.ie>
2018-10-02 13:57:45 +0100

    week 2

--- src/ca377/eatatdcu/urls.py
+++ src/ca377/eatatdcu/urls.py
@@ -4,9 +4,6 @@ from . import views
 app_name = 'eatatdcu'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-<<<<<<< HEAD
     url(r'^restaurants/', views.restaurants, name='restaurants'),
-=======
     url(r'^restaurants$', views.restaurants, name='restaurants'),
->>>>>>> upstream/master
 ]
--- src/ca377/eatatdcu/views.py
+++ src/ca377/eatatdcu/views.py
@@ -5,8 +5,6 @@ from django.template import loader
 def index(request):
     template = loader.get_template('eatatdcu/index.html')
     return HttpResponse(template.render({},request))
-def restaurants(request):
-   return render(request, 'eatatdcu/restaurants.html')
 
 def restaurants(request):
     template = loader.get_template('eatatdcu/restaurants.html')
