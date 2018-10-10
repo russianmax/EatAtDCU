f4e55be5987b596f5e8ca3bb1c6583f1f040edcf
Maksims Kompanijecs <kompanijecs.maksims2@mail.dcu.ie>
2018-10-10 11:28:58 +0100

    homepage

--- src/ca377/ca377/urls.py
+++ src/ca377/ca377/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^eatatdcu/', include('eatatdcu.urls')),
+    url(r'^', include('eatatdcu.urls')),
     url(r'^admin/', admin.site.urls),
 ]
